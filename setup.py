33e2113ca26e001808bf5deb4c772767b16aae73
Alexander <alexander.waldin@gmail.com>
2015-09-03 13:29:48 -0400

    updated setup.py with project url

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(name='projectname',
       version='0.1',
       description='Demo Modules',
+      url='https://github.com/alexanderwaldin/projectname',
       author='The Author',
       author_email='theauthor@example.com',
       license='MIT',
